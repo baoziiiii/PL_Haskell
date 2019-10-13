ed131ade223fd0e69b3e0df96f5ce59660675aa6
Wenqiang Bao <35871778+baoziiiii@users.noreply.github.com>
2019-10-13 16:02:01 -0400

    Update 1.2_allTwoSum.hs

--- 1.2_allTwoSum.hs
+++ 1.2_allTwoSum.hs
@@ -1,9 +1,9 @@
-twoSum :: [Int]->[(Int,[(Int,Int)])]
-twoSum [] = []
-twoSum xs = let min = minimum $ [a+b | a<-xs,b<-xs,a<b]
+allTwoSum :: [Int]->[(Int,[(Int,Int)])]
+allTwoSum [] = []
+allTwoSum xs = let min = minimum $ [a+b | a<-xs,b<-xs,a<b]
                 max = maximum $ [a+b | a<-xs,b<-xs,a<b]
                 twoSum :: Int -> (Int,[(Int,Int)])
                 twoSum n = (n,[(a,b)| a<-xs,b<-xs,a+b==n,a<b])               
             in [(twoSum x) | x<-[min..max]]
 
-main = print $ twoSum [1,3,8,12,7,11,9,4,2,10,5]
+main = print $ allTwoSum [1,3,8,12,7,11,9,4,2,10,5]
