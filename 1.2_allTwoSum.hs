2f82dd5ea8a04a45abeedd7209c2354545648c12
RyanBao <ryanbao@Ryans-Air.fios-router.home>
2019-10-13 16:06:13 -0400

    haskell

--- 1.2_allTwoSum.hs
+++ 1.2_allTwoSum.hs
@@ -1,9 +1,9 @@
 allTwoSum :: [Int]->[(Int,[(Int,Int)])]
 allTwoSum [] = []
 allTwoSum xs = let min = minimum $ [a+b | a<-xs,b<-xs,a<b]
-                max = maximum $ [a+b | a<-xs,b<-xs,a<b]
-                twoSum :: Int -> (Int,[(Int,Int)])
-                twoSum n = (n,[(a,b)| a<-xs,b<-xs,a+b==n,a<b])               
-            in [(twoSum x) | x<-[min..max]]
+                   max = maximum $ [a+b | a<-xs,b<-xs,a<b]
+                   twoSum :: Int -> (Int,[(Int,Int)])
+                   twoSum n = (n,[(a,b)| a<-xs,b<-xs,a+b==n,a<b])               
+                in [(twoSum x) | x<-[min..max]]
 
 main = print $ allTwoSum [1,3,8,12,7,11,9,4,2,10,5]
