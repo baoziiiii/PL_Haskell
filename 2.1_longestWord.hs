433d66a5150e84d9973006884d305183a24b6ce2
Wenqiang Bao <35871778+baoziiiii@users.noreply.github.com>
2019-10-13 16:01:09 -0400

    Update 2.1_longestWord.hs

--- 2.1_longestWord.hs
+++ 2.1_longestWord.hs
@@ -1,9 +1,9 @@
 longestWord :: String-> String
 longestWord "" = ""
 longestWord s = let wordls = words s
-                    strLength :: [String] -> [Int] 
-                    strLength []=[]
-                    strLength (x:xs) =  (length x):strLength xs
+                    wordLength :: [String] -> [Int] 
+                    wordLength []=[]
+                    wordLength (x:xs) =  (length x):wordLength xs
                 in snd $ maximum $ [(length word,word)|word <- wordls]
 
 main = print $ longestWord "This is programming in Haskell" 
